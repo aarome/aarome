23167cad03a5849402310f5de4f212e5c210875f
Jason Loeffler <j@minorscience.com>
2019-03-28 18:12:35 -0400

    date filter

---
+++ web/modules/custom/search_modal/src/Controller/SearchModalController.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * SearchModalController class.
+ */
+
+namespace Drupal\search_modal\Controller;
+
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\OpenModalDialogCommand;
+use Drupal\Core\Controller\ControllerBase;
+
+class SearchModalController extends ControllerBase {
+
+  public function modal() {
+    $options = [
+      'dialogClass' => 'popup-dialog-class',
+      'width' => '100%',
+      'height' => '100%'
+    ];
+    $response = new AjaxResponse();
+    $response->addCommand(new OpenModalDialogCommand(t('Modal title'), t('The modal text'), $options));
+
+    return $response;
+  }
+}
